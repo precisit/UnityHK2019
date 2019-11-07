55132b6e80ab0cb763c139cc3a2e0685bbf35ec8
Jonas Holgersson <holgersson1988@hotmail.com>
2019-11-07 19:13:53 +0100

    First Movement

---
+++ TopDown/Assets/_Scripts/FollowObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    public GameObject Follow;
+    public Vector3 Offset;
+    public bool LookAt;
+
+    void LateUpdate()
+    {
+        transform.position = Follow.transform.position + Offset;
+        if (LookAt){
+            transform.rotation = Quaternion.LookRotation((Follow.transform.position - transform.position), Vector3.up);
+        }
+    }
+}
---
+++ TopDown/Assets/_Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody myRigidbody;
+    public Camera mainCamera;
+    public LayerMask mask;
+
+    private void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        UpdateMovement();
+        UpdateRotation();
+
+        UpdateShooting();
+    }
+
+    private void UpdateMovement()
+    {
+        // Movement
+        Vector3 input = new Vector3( 
+                            (Input.GetKey(KeyCode.A) ? -1 : 0) + (Input.GetKey(KeyCode.D) ? 1f : 0f), 
+                            0f, 
+                            Input.GetKey(KeyCode.W) ? 1f : 0f + (Input.GetKey(KeyCode.S) ? -1f : 0f));
+        myRigidbody.AddForce(moveSpeed * input, ForceMode.Impulse);
+    }
+
+    private void UpdateRotation()
+    {
+        // Rotation Raycast
+        RaycastHit hit;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, float.MaxValue, mask))
+        {
+            Vector3 direction = Utils.Flatten(hit.point) - Utils.Flatten(transform.position);
+            transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+        }
+    }
+
+    private void UpdateShooting()
+    {
+        
+    }
+}
