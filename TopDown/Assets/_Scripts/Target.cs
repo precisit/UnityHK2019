71ce5a31231d79f005ab6c150bd3ecbda6d4bc3b
Jonas Holgersson <holgersson1988@hotmail.com>
2019-11-16 16:03:12 +0100

    Basic NavMesh

--- TopDown/Assets/_Scripts/Target.cs
+++ TopDown/Assets/_Scripts/Target.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Target : Damageable
 {
 
     public GameObject HitEffect;
+    
+    private GameObject Chase;
+    private NavMeshAgent agent;
 
     void Start()
     {
+        Player p = FindObjectOfType<Player>();
+        Chase = p.gameObject;
+        agent = GetComponent<NavMeshAgent>();
         
     }
 
     void Update()
     {
-        
+        agent.SetDestination(Chase.transform.position);
     }
 
     public override void OnDamage() {
