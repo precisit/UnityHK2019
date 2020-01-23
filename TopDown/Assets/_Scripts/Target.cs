21b08ac7f1e643f4262034839c7b36decf11640f
Simon Gollbo <sim.gollbo@gmail.com>
2020-01-23 19:55:43 +0100

    Add zombie sounds to Target. Add Zombie sounds asset

--- TopDown/Assets/_Scripts/Target.cs
+++ TopDown/Assets/_Scripts/Target.cs
@@ -7,20 +7,36 @@ public class Target : Damageable
 {
 
     public GameObject HitEffect;
-
+    public AudioClip[] PassiveSounds;
+    
     private GameObject Chase;
     private NavMeshAgent agent;
+    private AudioSource audioSource;
+    private float audioTime;
 
     void Start()
     {
         Player p = FindObjectOfType<Player>();
         Chase = p.gameObject;
         agent = GetComponent<NavMeshAgent>();
+        audioSource = GetComponent<AudioSource>();
+        audioTime = 0;
     }
 
     void Update()
     {
         agent.SetDestination(Chase.transform.position);
+
+        audioTime += Time.deltaTime;
+        if(audioTime > 10f) {
+            audioTime = 0;
+            if(Random.value < 0.5f) {
+                audioSource.pitch = Random.Range(0.8f, 1.2f);
+                audioSource.PlayOneShot(PassiveSounds[(int) Random.value*PassiveSounds.Length]);
+            }
+            
+        }
+
     }
 
     public override void OnDamage() {
